6e003160b834a3664b5fdf9ea417d875affefabb
tad dy <a@a.com>
2024-08-30 00:35:03 -0400

    save progress

--- src/anthropic_test.py
+++ src/anthropic_test.py
@@ -1,5 +1,5 @@
 import os
-
+import asyncio
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -8,7 +8,9 @@ from swarms.models import Anthropic
 # Initialize an instance of the Anthropic class
 model = Anthropic(anthropic_api_key=os.getenv("ANTHROPIC_API_KEY"), streaming=True)
 
-# Using the run method
+async def main():
 
-completion_1 = model.run("What is the capital of France? Answer in 100 to 200 words.")
-print(completion_1)
+  async for evt in  model.astream_events("What is the capital of France? Answer in 100 to 200 words.", version="v1"):
+    print(evt)
+
+asyncio.run(main())
